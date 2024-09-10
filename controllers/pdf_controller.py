b4030cc5f24c055dd1b7faf5a70736fc1029f3ba
Aman Jain <aman@tokeet.com>
2024-09-10 17:47:30 +0530

    Use OpenAI to get results

--- controllers/pdf_controller.py
+++ controllers/pdf_controller.py
@@ -1,21 +1,19 @@
 import os
-import json
-import time
-import requests
 import structlog
 
 from flask import Blueprint, request, jsonify
 from flask_pydantic import validate
 from langchain_community.document_loaders import PyPDFLoader
-from langchain.prompts import PromptTemplate
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain.vectorstores import FAISS
 from langchain.text_splitter import CharacterTextSplitter
+from langchain.chains import RetrievalQA
+from langchain.llms import OpenAI
 
 from models.pdf_model import PDFModel, QuestionsModel
 from utils.file_utils import allowed_file, is_valid_pdf
 from config.config import TEXT_SPLITTER, SUCCESS_STATUS, FAILED_STATUS, EM_ENCODE_KWARGS, EM_MODEL_KWARGS, EM_MODEL_NAME
-from templates.template import template
+from templates.template import prompt
 
 pdf_apis = Blueprint('pdf', __name__)
 
@@ -50,7 +48,7 @@ def upload_pdf():
 
         embeddings = HuggingFaceEmbeddings(model_name=EM_MODEL_NAME, model_kwargs=EM_MODEL_KWARGS, encode_kwargs=EM_ENCODE_KWARGS)
         vectorstore = FAISS.from_documents(documents, embeddings)
-        vectorstore.save_local("faiss_index_constitution")
+        vectorstore.save_local("faiss_pdf_vectorstore")
 
         return jsonify({"filename": file_name, "status": SUCCESS_STATUS})
 
@@ -63,43 +61,18 @@ def ask_questions():
     try:
         data = request.get_json()
         questions = data.get('questions', [])
-
-        prompt = PromptTemplate(
-            input_variables=["context", "question"],
-            template=template,
-        )
         answers = {}
         embeddings = HuggingFaceEmbeddings(model_name=EM_MODEL_NAME, model_kwargs=EM_MODEL_KWARGS, encode_kwargs=EM_ENCODE_KWARGS)
-        persisted_vectorstore = FAISS.load_local("faiss_index_constitution", embeddings, allow_dangerous_deserialization=True)
+        persisted_vectorstore = FAISS.load_local("faiss_pdf_vectorstore", embeddings, allow_dangerous_deserialization=True)
         for question in questions:
             relevant_docs = persisted_vectorstore.similarity_search(query=question, fetch_k=1)
             logger.info(f"======{relevant_docs}")
             context = relevant_docs[0].page_content
             filled_prompt = prompt.format(question=question, context=context)
-            
-            request_payload = {
-                "model": "llama2",
-                "prompt": filled_prompt,
-                "format": "json"
-            }
-            answers[question] = get_answer(request_payload)
+            qa = RetrievalQA.from_chain_type(llm=OpenAI(), chain_type="stuff", retriever=persisted_vectorstore.as_retriever())
+            result = qa.run(filled_prompt)
+            answers[question] = result
         return jsonify(answers), 200
 
     except Exception as e:
         return jsonify({'error': str(e)}), 400
-
-def get_answer(request_payload):
-    try:
-        response = requests.post("http://localhost:11434/api/generate", json=request_payload, timeout=60.0)
-
-        def stream_response_generator():
-            response_str = ""
-            for chunk in response.iter_lines():
-                logger.info(f"Received chunk: {chunk}")
-                json_data = json.loads(chunk)
-                response_str += json_data['response'] + " "
-                time.sleep(1) 
-            return response_str
-        return stream_response_generator()
-    except Exception as e:
-        return "could not find the answer!"
--- templates/template.py
+++ templates/template.py
@@ -1,6 +1,5 @@
-template = """[INST] <<SYS>> Use the following pieces of context to answer the question at the end. 
-If you don't know the answer, just say that you don't know, don't try to make up an answer. 
-Use three sentences maximum and keep the answer as concise as possible. <</SYS>>
-{context}
+prompt = """Use the following pieces of context to answer the question at the end. 
+If you don't know the answer or have low confidence, just say that Data Not Available, don't try to make up an answer.
+context: {context}
 Question: {question}
-Helpful Answer:[/INST]"""
+"""
