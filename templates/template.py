9e83142b03be30dd1c1a496064a808e5af8c0b64
Aman Jain <amanjain5221@gmail.com>
2024-09-11 12:40:50 +0530

    Update prompt

--- templates/template.py
+++ templates/template.py
@@ -1,5 +1,6 @@
 prompt = """Use the following pieces of context to answer the question at the end. 
 If you don't know the answer or have low confidence, just say that Data Not Available, don't try to make up an answer.
-context: {context}
+Context: {context}
 Question: {question}
+Answer: 
 """
